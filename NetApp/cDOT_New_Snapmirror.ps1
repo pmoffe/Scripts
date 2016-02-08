b6c7459ce2b412c777b4ca91a095de5b76bf41f0
ad8mustanggt <mike@strategicinfosystems.net>
2016-02-08 14:49:05 -0500

    Updated to use functions

--- NetApp/cDOT_New_Snapmirror.ps1
+++ NetApp/cDOT_New_Snapmirror.ps1
@@ -1,40 +1,32 @@
 #SVM Configuratino Variables
-Write-Host "Loading Configuratino Variables..." -foregroundcolor "green"
-    $site_id =	Read-Host -Prompt 'What Site are you adding SnapMirrors to?'
-    $cust_id =	Read-Host -Prompt 'What Client are these new SnapMirrors for? (use the 3 character client ID)'
-    $cust_svm = Read-Host -Prompt 'What is the destination SVM Name?'
-    $agg = Read-Host -Prompt 'What Aggregate should the snapmirror volumes be placed on?'
+$site_id =	Read-Host -Prompt 'What Site are you adding SnapMirrors to?'
+$cust_id =	Read-Host -Prompt 'What Client are these new SnapMirrors for? (use the 3 character client ID)'
+$cust_svm = Read-Host -Prompt 'What is the destination SVM Name?'
+$agg = Read-Host -Prompt 'What Aggregate should the snapmirror volumes be placed on?'
 
 
-#-----------------------------------------
-# Set-up static variables
-#-----------------------------------------
-
-# Set internal variables
-Write-Host "Loading Internal Variables..." -foregroundcolor "green"
-    $snapcsv = Import-Csv .\cDOT_New_Snapmirror.csv
-
-# Set NetApp Cluster IP based on site ID
-if($site_id -eq "PHL-07") {$filer = "10.30.0.133" }
-if($site_id -eq "PHL-14") {$filer = "10.30.0.90" }
-if($site_id -eq "PHX-55") {$filer = "10.35.0.90" }
-
-#-----------------------------------------
-# No configurable variables past this point
-#-----------------------------------------
+#-------------------------------------------#
+# No configurable variables past this point #
+#-------------------------------------------#
 
 #Run initial checks
 if(-not $site_id -or -not $cust_id -or -not $cust_svm -or -not $agg) {
 	Write-Host "Problem with Variables, I quit." -foregroundcolor "red"
 	exit
-	}
+}
 
-#Load the DataONTAP Module
-Import-Module DataONTAP
+# Set internal variables
+Write-Host "Importing the cDOT_New_Snapmirror.csv file..." -foregroundcolor "green"
+    $snapcsv = Import-Csv $PSScriptRoot\cDOT_New_Snapmirror.csv
 
-#Connect to NetApp
-Write-Host "Connecting to $filer..." -foregroundcolor "green"
-    Connect-NcController $filer -vserver $cust_svm | Out-Null
+# Import custom NetApp Functions
+Import-Module $PSScriptRoot\NetApp_Functions.psm1
+
+# Load NetApp Config
+Get-NAConfig
+
+# Connect to NetApp
+Connect-NcFiler -ncfiler $Global:naconfig.$site_id
 
 #Create Volume(s)
 Write-Host "Creating Volume(s)..." -foregroundcolor "green"
