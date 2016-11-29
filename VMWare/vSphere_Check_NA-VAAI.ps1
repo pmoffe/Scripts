01c5b34e8edd4e8b99b5834c4b67594205612ae1
ad8mustanggt <mike@strategicinfosystems.net>
2016-11-29 17:22:12 -0500

    Loop

--- VMWare/vSphere_Check_NA-VAAI.ps1
+++ VMWare/vSphere_Check_NA-VAAI.ps1
@@ -1,21 +1,25 @@
-$vCenter = Read-Host -Prompt 'What vCenter do you want to check?'
+$vCenters = Read-Host -Prompt 'What vCenter do you want to check?'
 
 # Import custom VMWare Functions
 Import-Module $PSScriptRoot\VMWare_Functions.psm1
 
-# Connect to vCenter
-Connect-viserver $vCenter
+Foreach ($vCenter in $vCenters) {
 
-Foreach ($vmhost in Get-VMHost) {
-     $esxcli = Get-EsxCli -vmhost $vmhost
-     # check that netapp vib is installed
-     $vsc = $esxcli.software.vib.list() | Where-Object { $_.Name -eq "NetAppNasPlugin" }
-     if ($vsc) {
-        "Found " + $vsc.id + " on " + $vmhost.Name
-     } else {
-        Write-Host "NetAppNasPlugin missing from" $vmhost.Name -foregroundcolor "red"
-     }
+  # Connect to vCenter
+  Connect-viserver $vCenter
+
+  Foreach ($vmhost in Get-VMHost) {
+       $esxcli = Get-EsxCli -vmhost $vmhost
+       # check that netapp vib is installed
+       $vsc = $esxcli.software.vib.list() | Where-Object { $_.Name -eq "NetAppNasPlugin" }
+       if ($vsc) {
+          "Found " + $vsc.id + " on " + $vmhost.Name
+       } else {
+          Write-Host "NetAppNasPlugin missing from" $vmhost.Name -foregroundcolor "red"
+       }
+  }
+
+  #Disconnect vCenter
+  disconnect-viserver -Confirm:$false
+
 }
-
-#Disconnect vCenter
-disconnect-viserver -Confirm:$false
